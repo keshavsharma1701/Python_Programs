caaf6622a72c1201f19e96fcfa79bf200a91fd9a
Keshav Sharma <keshav93104@gmail.com>
2022-10-03 21:25:37 +0530

    keshav

---
+++ Syntax_Sample.py
@@ -0,0 +1,37 @@
+print("welcome in the program:")
+a=34
+print(a*10)
+print("this program is made by KESHAV SHARMA :")
+v=input("enter any no")
+print("copyright ")
+print("thanking you ")
+rt5 ,lrt6=87,986
+language="python"
+language='keshav'
+language="""keshav
+shar
+ma"""
+z=True
+y=rt5+lrt6
+z=2*y
+print("hello",rt5,lrt6,"hi",language,z)
+print(type(language),y,z)
+pl1, pl2, pl3= "keshav", "shar", "uyg"
+print(pl1, pl2, pl3)
+pl1=pl3="hi"
+print(pl1,"    ",pl2, pl3)
+num =[9 , 8 , 7]
+print(num[2])
+a=9
+b=8
+c=6
+print(a**b)
+kes=input()
+'''ouglycuyfiyctc
+iugouvou'''
+if 4>6:
+    print("dfhdfh")
+    print("tcccccccccccuccuc")
+print("jvjcc")
+d1={"name":"keshav sharma","age":"18"}
+print(d1["age"])
